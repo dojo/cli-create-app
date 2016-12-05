dffba1ec214cf55acf6966550b5dab712d244464
James Donaghue <james.donaghue@gmail.com>
2016-12-05 10:45:06 -0500

    Fix help so it displays per grouping

--- src/register.ts
+++ src/register.ts
@@ -1,13 +1,8 @@
-import { Helper } from '@dojo/cli/interfaces';
-import { Yargs } from 'yargs';
-
-export default function(helper: Helper): Yargs {
-	helper.yargs.option('n', {
+export default function(options: (key: string, options: Options) => void): void {
+	options('n', {
 		alias: 'name',
 		describe: 'The name of your application',
 		demand: true,
 		type: 'string'
 	});
-
-	return helper.yargs;
 }
