9eb9a568511d40595026e5527c718eefca1026ff
James Donaghue <james.donaghue@gmail.com>
2016-12-29 16:29:29 -0500

    Update tests

--- tests/unit/register.ts
+++ tests/unit/register.ts
@@ -2,6 +2,7 @@ import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import register from './../../src/register';
 import * as sinon from 'sinon';
+import { getHelperStub } from '../support/testHelper';
 
 let sandbox: sinon.SinonSandbox;
 
@@ -15,13 +16,13 @@ registerSuite({
 	},
 	'Should add a yargs option for name'() {
 		const options = sandbox.stub();
-		register(options);
+		register(getHelperStub<any>(), options);
 		assert.isTrue(options.calledOnce);
 		assert.isTrue(options.firstCall.calledWithMatch('n', { 'alias': 'name' }));
 	},
 	'Should demand the name option'() {
 		const options = sandbox.stub();
-		register(options);
+		register(getHelperStub<any>(), options);
 		assert.isTrue(options.firstCall.calledWithMatch('n', { 'demand': true }));
 	}
 });
