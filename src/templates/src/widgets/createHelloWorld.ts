8d7795701c677e5be29ac6a817b43299a0ce43c5
Anthony Gubler <agubler@sitepen.com>
2017-02-02 17:43:13 +0000

    update to latest dojo2

--- src/templates/src/widgets/createHelloWorld.ts
+++ src/templates/src/widgets/createHelloWorld.ts
@@ -2,10 +2,8 @@ import createWidgetBase from '@dojo/widget-core/createWidgetBase';
 import { v } from '@dojo/widget-core/d';
 
 const createHelloWorld = createWidgetBase.override({
-	getChildrenNodes() {
-		return [
-			v('div', [ 'Hello, Dojo World!' ])
-		];
+	render() {
+		return v('div', [ 'Hello, Dojo World!' ]);
 	}
 });
 
