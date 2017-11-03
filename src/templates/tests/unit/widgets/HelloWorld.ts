11a61d55130b806b8677bb64eb763a21f982cd17
Ed Hager <ehager@sitepen.com>
2017-11-03 09:48:30 -0700

    Update the created app to use the Intern BDD API rather than the object API to remove any confusion over the need to have a "tests" property in the object version of the tests.

--- src/templates/tests/unit/widgets/HelloWorld.ts
+++ src/templates/tests/unit/widgets/HelloWorld.ts
@@ -1,4 +1,4 @@
-const { registerSuite } = intern.getInterface('object');
+const { describe, it } = intern.getInterface('bdd');
 import harness from '@dojo/test-extras/harness';
 
 import { v } from '@dojo/widget-core/d';
@@ -6,12 +6,12 @@ import { v } from '@dojo/widget-core/d';
 import HelloWorld from '../../../src/widgets/HelloWorld';
 import * as css from '../../../src/widgets/styles/HelloWorld.m.css';
 
-registerSuite('HelloWorld', {
-	'render'() {
+describe('HelloWorld', () => {
+	it('should render widget', () => {
 		const testHelloWorld = harness(HelloWorld);
 		testHelloWorld.expectRender(v('div', { classes: testHelloWorld.classes(css.root) }, [
 			v('img', { src: './img/logo.svg', classes: testHelloWorld.classes(css.logo) }),
 			v('div', { classes: testHelloWorld.classes(css.label) }, [ 'Hello, Dojo 2 World!' ])
 		]));
-	}
+	});
 });
