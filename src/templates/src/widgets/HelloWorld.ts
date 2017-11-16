a2f47fec3341f4378e0a46ace548aff802ba4be1
Anthony Gubler <agubler@sitepen.com>
2017-11-16 14:26:30 +0000

    Upgrade for latest dojo dependencies

--- src/templates/src/widgets/HelloWorld.ts
+++ src/templates/src/widgets/HelloWorld.ts
@@ -1,5 +1,4 @@
 import { v } from '@dojo/widget-core/d';
-import { ThemeableMixin, theme } from '@dojo/widget-core/mixins/Themeable';
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 
 import * as css from './styles/HelloWorld.m.css';
@@ -10,19 +9,17 @@ import * as css from './styles/HelloWorld.m.css';
  *
  * Refer to these tutorials for more help with creating a themed widget:
  * 	- Creating widgets, https://dojo.io/tutorials/003_creating_widgets/
- * 	- Theming widgets, https://dojo.io/tutorials/007_theming/
  */
-@theme(css)
-export class HelloWorld extends ThemeableMixin(WidgetBase) {
+export class HelloWorld extends WidgetBase {
 	/**
 	 * Override WidgetBase#render to produce a virtual DOM tree.
 	 * @returns {HNode} Each time render() executes, it should build the entire virtual DOM tree.
 	 */
 	protected render() {
 		// Use WidgetBase#classes() to assign CSS classnames from the theme to the virtual DOM nodes.
-		return v('div', { classes: this.classes(css.root) }, [
-			v('img', { src: './img/logo.svg', classes: this.classes(css.logo) }),
-			v('div', { classes: this.classes(css.label) }, [ 'Hello, Dojo 2 World!' ])
+		return v('div', { classes: css.root }, [
+			v('img', { src: './img/logo.svg', classes: css.logo }),
+			v('div', { classes: css.label }, [ 'Hello, Dojo 2 World!' ])
 		]);
 	}
 }
