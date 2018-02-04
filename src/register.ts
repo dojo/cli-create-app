865f5359afeb97b8a27ec5636da78690ec950fe7
Tom Dye <tomdye@users.noreply.github.com>
2018-02-04 10:20:59 +0000

    use version 0.6.0 of cli without cli-create-app, change interfaces usage (#138)

--- src/register.ts
+++ src/register.ts
@@ -1,4 +1,4 @@
-import { OptionsHelper } from '@dojo/interfaces/cli';
+import { OptionsHelper } from '@dojo/cli/interfaces';
 
 export default function(options: OptionsHelper): void {
 	options('n', {
