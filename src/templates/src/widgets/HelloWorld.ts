ecc575eadaaf5837c421d26ba6c85b28e3980543
Anthony Gubler <agubler@sitepen.com>
2018-02-11 21:58:24 +0000

    Update for upcoming Dojo RC release

--- src/templates/src/widgets/HelloWorld.ts
+++ src/templates/src/widgets/HelloWorld.ts
@@ -1,24 +1,17 @@
+import WidgetBase from '@dojo/widget-core/WidgetBase';
 import { v } from '@dojo/widget-core/d';
-import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 
 import * as css from './styles/helloWorld.m.css';
 
 const logo = require('./../img/logo.svg');
 
 /**
- * A themed "Hello World" widget that renders a spinning Dojo 2 logo and the text
- * "Hello, Dojo 2 World!".
+ * A "Hello World" widget that renders a spinning Dojo 2 logo and the text "Hello, Dojo 2 World!".
  *
- * Refer to these tutorials for more help with creating a widget:
- * 	- Creating widgets, https://dojo.io/tutorials/003_creating_widgets/
+ * Refer to the creating widgets tutorial for help: https://dojo.io/tutorials/003_creating_widgets/
  */
 export class HelloWorld extends WidgetBase {
-	/**
-	 * Override WidgetBase#render to produce a virtual DOM tree.
-	 * @returns {HNode} Each time render() executes, it should build the entire virtual DOM tree.
-	 */
 	protected render() {
-		// Use WidgetBase#classes() to assign CSS classnames from the theme to the virtual DOM nodes.
 		return v('div', { classes: css.root }, [
 			v('img', { src: logo, classes: css.logo }),
 			v('div', { classes: css.label }, ['Hello, Dojo 2 World!'])
