454bbf1d2123454900cc621ec2bc2f5061e85b67
rorticus <rmulligan@sitepen.com>
2020-03-30 12:45:14 -0400

    Updating to create apps based on dojo 7 alphas (#178)
    * Updating lint-staged
    
    * Making template package.json spacing consistent

--- src/templates/ts/standard/src/main.ts
+++ src/templates/ts/standard/src/main.ts
@@ -1,7 +1,6 @@
 import renderer, { w } from '@dojo/framework/core/vdom';
 import Registry from '@dojo/framework/core/Registry';
 import { registerRouterInjector } from '@dojo/framework/routing/RouterInjector';
-import '@dojo/themes/dojo/index.css';
 
 import routes from './routes';
 import App from './App';
